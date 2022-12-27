4125e4d6b9318b8e4acf5d202a820bd5f690720d
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-12-27 21:27:39 +0100

    day15 parse and print ok for part 1

--- day15/main.cpp
+++ day15/main.cpp
@@ -37,12 +37,12 @@ std::pair<std::string,std::string> to_splitted(std::string const& s,std::string 
   }
 }
 
-using Result = size_t;
+using Result = long long int;
 using Answers = std::vector<std::pair<std::string,Result>>;
 
 struct Vector {
-  int row;
-  int col;
+  Result row;
+  Result col;
   bool operator<(Vector const& other) const {
     if (row == other.row) return col < other.col;
     else return row < other.row;
@@ -90,8 +90,9 @@ struct Model {
 };
 
 std::ostream& operator<<(std::ostream& os,Model const& model) {
-  const int frame=3;
+  const Result frame=3;
   os << "\n<map> top_left:" << model.top_left << " bottom_right:" << model.bottom_right;
+  return os;
   for (auto row=model.top_left.row-frame;row<=model.bottom_right.row+frame;++row) {
     os << "\n";
     for (auto col=model.top_left.col-frame;col<=model.bottom_right.col+frame;++col) {
@@ -176,7 +177,7 @@ int main(int argc, char *argv[])
 {
   Answers answers{};
   answers.push_back({"Part 1 Test",part1::solve_for(pTest)});
-  // answers.push_back({"Part 1     ",part1::solve_for(pData)});
+  answers.push_back({"Part 1     ",part1::solve_for(pData)});
   // answers.push_back({"Part 2 Test",part2::solve_for(pTest)});
   // answers.push_back({"Part 2     ",part2::solve_for(pData)});
   for (auto const& answer : answers) {
